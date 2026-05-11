836726ae983da43c0d26cd885d9b9af4c41390f2
eloisagaaby <eloisagabriele2006@gmail.com>
2026-05-11 20:13:07 -0300

    Add files via upload

---
+++ Calculadora em PHP/GET.php
@@ -0,0 +1,8 @@
+<?php
+        $a = $_GET['num_a'];
+        $b = $_GET['num_b'];
+        $resultado = $a + $b;
+
+        echo "O valor da soma de $a e $b é $resultado"
+        
+?>
---
+++ Calculadora em PHP/POST.php
@@ -0,0 +1,8 @@
+<?php
+        $a = $_POST['num_a'];
+        $b = $_POST['num_b'];
+        $resultado = $a + $b;
+
+        echo "O valor da soma de $a e $b é $resultado"
+        
+?>
